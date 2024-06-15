cee731398a45d95ec5183b9b806dc88c316d86ca
xptea <webster30956@proton.me>
2024-06-15 11:51:19 -0400

    #

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('fuck you');
+  res.end('fuck youc');
 });
 
 server.listen(PORT, () => {
