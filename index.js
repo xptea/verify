4500deee7cd150e94c3656375388e6c8023187d3
xptea <webster30956@proton.me>
2024-06-15 11:42:12 -0400

    FU

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end('fuck you');
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`S,knmzdx rfvtbgmzhbg,kjhdf vbngnzd bknm,vgbdn, mgvf,hbv gfnsrzhbvga,edbfghttp://localhost:${PORT}/`);
 });
